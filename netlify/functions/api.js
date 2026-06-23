093cccea0c733898b3489bede93a220fe7779ab7
Zayd Faisal <mzaydf@gmail.com>
2026-06-23 09:54:37 +0530

    Simplify Netlify function handler

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../../server');
 
-const handler = serverless(app, {
-  request(req, event) {
-    // Netlify passes the original path in event.path
-    // Make sure Express sees the correct URL
-    req.url = event.path + (event.rawQuery ? '?' + event.rawQuery : '');
-  }
-});
-
-exports.handler = handler;
+exports.handler = serverless(app);
